4b7ff0086bac769d0344b51551a72e9f2aa5bd70
Malek Ammar <Malek.ammar@etudiant-enit.utm.tn>
2020-09-20 14:21:51 +0100

    Updates

---
+++ precache-manifest.8d5a5cdf3ad106c8af801a81ec13a54f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eccb1a5954d596b3e413b40f1a6cce5",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "81dd7f596f416fff49fc",
+    "url": "/react-deploy/static/css/main.631965df.chunk.css"
+  },
+  {
+    "revision": "79d5610f2beb4a3884bb",
+    "url": "/react-deploy/static/js/2.0d2750ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.0d2750ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81dd7f596f416fff49fc",
+    "url": "/react-deploy/static/js/main.e1afcadc.chunk.js"
+  },
+  {
+    "revision": "be0af9760a9f59c6ba29",
+    "url": "/react-deploy/static/js/runtime-main.7a26a125.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
