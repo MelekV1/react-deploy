756c82d7d4508623c5519eb1562aa04d1a2848d0
Malek Ammar <Malek.ammar@etudiant-enit.utm.tn>
2020-09-20 14:42:43 +0100

    modification

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>App Coming Soon <span>IAS</span> </h1>
+      <h1>APP Going to be deployed soon </h1>
     </div>
   );
 }
